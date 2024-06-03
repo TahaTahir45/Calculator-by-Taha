b3fb69770b734caa887c4aa16134fda246314792
taha <tahatahir996@gmail.com>
2024-06-03 18:17:58 +0500

    Initial Commit

---
+++ Calculator by Taha/Calculator_by_TahaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_by_TahaApp.swift
+//  Calculator by Taha
+//
+//  Created by Taha Tahir on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_by_TahaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
